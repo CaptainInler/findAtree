890e68f32f01a10e74c6055ab8ba224cbc4b8b6c
Raluca <nicola.raluca@gmail.com>
2018-01-04 19:02:53 +0100

    Add map component
    https://github.com/CaptainInler/findAtree/issues/5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent {
   items: Observable<any[]>;
   constructor(db: AngularFireDatabase) {
     this.items = db.list('features', ref => ref.limitToFirst(10)).valueChanges();
+    console.log(this.items);
   }
+
 }
 
 
---
+++ src/app/map.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+import WebScene = require('esri/WebScene');
+import esriConfig = require('esri/config');
+
+@Injectable()
+export class MapService {
+  webscene: WebScene;
+  constructor() {
+    esriConfig.request.corsEnabledServers.push("a.tile.stamen.com", "b.tile.stamen.com", "c.tile.stamen.com", "d.tile.stamen.com");
+    this.webscene = new WebScene({
+      portalItem: {
+        id: 'c01e6ad0e45449e39354e08c1158e706'
+      }
+    });
+  }
+}
