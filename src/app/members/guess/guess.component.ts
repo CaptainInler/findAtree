7330ba7e8474075500e13c6d2d3afded248085e1
Beat Fleischli <b.fleischli@aui.ch>
2018-02-11 19:09:43 +0100

    no treename twice

--- src/app/members/guess/guess.component.ts
+++ src/app/members/guess/guess.component.ts
@@ -27,35 +27,16 @@ export class GuessComponent implements OnInit {
     this.eventData.emit(event);
   }
   ngOnInit() {
-    this.selection = this.tNS.getSelectionOfNames(this.authService.level-1);
-    let tree: FirebaseTN = {
+    let tree: Treename = {
       id: this.mapClickData.attr.ObjectID,
       baumartlat: this.mapClickData.attr.baumartlat,
       baumgattunglat: this.mapClickData.attr.baumgattunglat,
       baumnamelat: this.mapClickData.attr.baumnamelat,
       baumnamedeu: this.mapClickData.attr.baumnamedeu,
     };
+    this.selection = this.tNS.getSelectionOfNames(this.authService.level-1,tree);
     this.answer = this.mapClickData.attr.ObjectID;
-    this.selection.push(tree);
     // console.log(this.selection);
-    this.selection = this.shuffle(this.selection);
-    // console.log(this.selection);
-  }
-  shuffle(array) {
-    var m = array.length, t, i;
-
-    // While there remain elements to shuffle…
-    while (m) {
-
-      // Pick a remaining element…
-      i = Math.floor(Math.random() * m--);
-
-      // And swap it with the current element.
-      t = array[m];
-      array[m] = array[i];
-      array[i] = t;
-    }
-    return array;
   }
   selectTreeName(id: number) {
     if (id===this.answer){
--- src/app/services/tree-name.service.ts
+++ src/app/services/tree-name.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import {Injectable, EventEmitter, OnInit} from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import {Treename, FirebaseTN} from '../treename';
@@ -11,39 +11,82 @@ export class TreeNameService {
   constructor(private db: AngularFireDatabase) {
   }
 
-  public getSelectionOfNames(size: number): Array<Treename>{
+
+  public getSelectionOfNames (size: number, clicked: Treename = null): Array<Treename>{
     let set: Array<Treename> = Array();
-    if(this.dataLoaded) {
-      let length = this.treenames.length;
+    let clickedNameId: number = 0;
+    this.loadTreeNames().subscribe( (treenames) => {
+      // console.log(treenames);
+      let length = treenames.length;
+      if (clicked) {
+        clickedNameId = this.getTreenameId(clicked.baumnamelat, clicked.baumnamedeu);
+      }
       for (let i = 0; i < size; i++) {
         let index = this.getRandomInt(1, length);
-        set.push(this.treenames[index]);
+        while (index === clickedNameId){
+          index = this.getRandomInt(1, length);
+        }
+        set.push(treenames[index]);
       }
-    }
+      set.push(clicked);
+      set = this.shuffle(set);
+      console.log(set);
+    })
     return set;
   }
+  public getTreenameId(nameLat: string, nameDeu: string): number{
+    if (this.dataLoaded) {
+      let id: number = 0;
+      for (let i = 0; i < this.treenames.length; i++) {
+        if ((this.treenames[i].baumnamelat===nameLat)&&(this.treenames[i].baumnamedeu===nameDeu)){
+          id = this.treenames[i].id;
+          break;
+        }
+      }
+      return id;
+    }
+    return null;
+  }
 
-  public getSelectionOfNamesObs(size: number, selected: number): Observable<Treename[]>{
+  public loadTreeNames(): Observable<Treename[]>{
     if (this.dataLoaded){
       const obs =  Observable.create((observer) => {
         observer.next(this.treenames);
-        console.log('treenames read');
+        // console.log('treenames read');
         observer.complete();
       });
       return obs;
     }
-    this.db.list('treenames').valueChanges()
-      .subscribe(trees => {
-        this.treenames = trees.map((tree:FirebaseTN) => {
-          return new Treename(tree);
-        });
+    let dbs = this.db.list<Treename>('treenames').valueChanges();
+
+      dbs.subscribe(trees => {
+        this.treenames = trees
         this.dataLoaded = true;
+        // console.log(trees);
         return this.treenames;
       });
+    return dbs;
   }
 
   private getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  shuffle(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
   }
 
 
