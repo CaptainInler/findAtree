24ea0073ceefe3b3789a32fa3fae23ebb41bf9ca
Beat Fleischli <b.fleischli@aui.ch>
2018-03-10 22:28:50 +0100

    add best score, clean up guess-panel code

--- src/app/map/guess-panel/guess-panel.component.ts
+++ src/app/map/guess-panel/guess-panel.component.ts
@@ -2,37 +2,58 @@ import { Component, Input } from '@angular/core';
 import { MapDataService } from '../../services/map-data.service';
 import { Utils} from '../../classes/utils';
 import {Guess, Score} from '../../classes/guess';
-import {AngularFireDatabase} from 'angularfire2/database';
+import {AngularFireDatabase, AngularFireObject, AngularFireList} from 'angularfire2/database';
 import {AuthService} from '../../services/auth.service';
-import {OnChanges, OnDestroy} from '@angular/core';
+import {OnInit, OnChanges, OnDestroy} from '@angular/core';
+
+type scoreType = 'day' | 'total' | 'best';
 
 @Component({
   selector: 'guess-panel',
   templateUrl: './guess-panel.component.html',
   styleUrls: ['./guess-panel.component.scss']
 })
-export class GuessPanelComponent implements OnDestroy, OnChanges{
+export class GuessPanelComponent implements OnInit, OnChanges, OnDestroy{
 
   @Input() selectedTree;
   error: any;
   selection: Array<string> = [];
-  correctAnswer: boolean = false;
   buttonState = {};
   selectedTreeId: number;
   selectedTreeName: string;
-  points: number = 0;
+  private points: number = 0;
+  private dayScoreRef$: AngularFireObject<Score>;
+  private totScoreRef$: AngularFireObject<Score>;
+  private maxScoreRef$: AngularFireList<Score>;
+  private today: string = '';
+  private dayScore: number = 0;
+  private totScore: number = 0;
+  private maxScore: number = 0;
+
 
   constructor(private mapDataService: MapDataService,
               private _db: AngularFireDatabase,
               private _aS: AuthService) { }
 
+  ngOnInit(){
+    // let db = this._db.list<Score>('score/total',ref=>ref.orderByChild('p').limitToLast(1)).valueChanges().subscribe(last=>{
+    //   console.log(last);
+    //   if(last) {
+    //     this.maxScore = last[0].p;
+    //   }
+    // });
+
+    this.today = Utils.getDate();
+    this.dayScoreRef$ = this._db.object<Score>(`score/day/${this._aS.getUserId()}/${this.today}`);
+    this.totScoreRef$ = this._db.object<Score>(`score/total/${this._aS.getUserId()}`);
+    this.maxScoreRef$ = this._db.list<Score>('score/total',ref=>ref.orderByChild('p').limitToLast(1));
+    this.setScoreRefs();
+   }
+
   ngOnChanges(values) {
     console.log(values);
-    if (!(this.points === 0) ) {
-      this.updateGuess();
-    }
+    this.updateGuess(this.points);
     if (values.selectedTree.currentValue) {
-      this.correctAnswer = false;
       this.selectedTreeId = values.selectedTree.currentValue.attributes.OBJECTID;
       this.selectedTreeName = values.selectedTree.currentValue.attributes.baumnamede;
       this.selection = this.mapDataService.getRandomTreeNames(this._aS.level,this.selectedTreeName );
@@ -45,9 +66,7 @@ export class GuessPanelComponent implements OnDestroy, OnChanges{
 
   ngOnDestroy(){
     console.log('on destroy');
-    if (!(this.points === 0) ) {
-      this.updateGuess();
-    }
+      this.updateGuess(this.points);
   }
 
   initButtonState() {
@@ -58,74 +77,92 @@ export class GuessPanelComponent implements OnDestroy, OnChanges{
 
   selectTreeName(name: string, event) {
     if (this.selectedTree.attributes.baumnamede === name) {
-      this.correctAnswer = true;
       this.points += this._aS.level;
       this.buttonState[name] = 'correct';
     } else {
-      this.correctAnswer = false;
       this.points--;
       this.buttonState[name] = 'false';
     }
   }
 
-  getScore(period: string): number{
-    if(period==='day'){
-      return this._aS.dayScore;
-    }else{
-      return this._aS.totScore;
+  setScoreRefs(){
+    this.dayScoreRef$.valueChanges().subscribe(last=>{
+      if(last) {
+        this.dayScore = last.p;
+      }
+    });
+    this.totScoreRef$.valueChanges().subscribe(last=>{
+      if(last) {
+        this.totScore = last.p;
+      }
+    });
+    this.maxScoreRef$.valueChanges().subscribe(last=>{
+      console.log(last);
+      if(last.length > 0) {
+        this.maxScore = last[0].p;
+      }
+    })
+  }
+
+  getScore(period: scoreType): number{
+    let score: number;
+    switch (period){
+      case 'day':
+        score = this.dayScore;
+        break;
+      case 'total':
+        score = this.totScore;
+        break;
+      case 'best':
+        score = this.maxScore;
+        break;
+      default:
+        score = null;
+    }
+    return score;
+  }
+
+  updateGuess(points: number) {
+    if (points!==0) {
+      let guess: Guess = {
+        treeId: this.selectedTreeId,
+        treeNameId: this.selectedTreeName,
+        points: this.points
+      };
+      this.setGuess(guess);
+      this.updateScore('day', points);
+      this.updateScore('total', points);
     }
   }
 
-  updateGuess() {
-    let guess: Guess = {
-      treeId : this.selectedTreeId,
-      treeNameId: this.selectedTreeName,
-      points: this.points
-    };
-    let d = Utils.getDate();
+  setGuess ( guess: Guess){
     let t = Utils.getTime();
-    console.log(d+" "+t);
-    let refLast = this._db.object(`guess/${this._aS.getUserId()}/${d}/${t}`);
-    let refScore = this._db.object<Score>(`guess/${this._aS.getUserId()}/${d}/score`);
-    let refTot = this._db.object<Score>(`guess/${this._aS.getUserId()}/score`);
-    refLast.valueChanges().take(1)
-      .subscribe(last => {
-        if (!last) {
-          refLast.update(guess)
+    let refLast = this._db.object(`guess/${this._aS.getUserId()}/${this.today}/${t}`);
+    refLast.set(guess)
             .catch( (err) =>
-              console.log(err));
-        }
-      });
-    refScore.valueChanges().take(1)
-      .subscribe( score => {
-        let s: Score = {p:guess.points};
-        if (score) {
-          s.p += score.p;
-        }
-        refScore.update(s)
-          .then( res => {
-            this._aS.dayScore = s.p;
-          })
-          .catch((err) => {
-              console.log(err);
-            }
-          );
-      });
-    refTot.valueChanges().take(1)
-      .subscribe(tot => {
-        let t: Score = {p:guess.points};
-        if (tot) {
-          t.p += tot.p;
-        }
-        refTot.update(t)
-          .then( res => {
-            this._aS.totScore = t.p;
-          })
-          .catch((err) => {
-              console.log(err);
-            }
-          );
-      })
+              console.log(err)
+            );
+  }
+
+  updateScore(period: scoreType, points: number = 0){
+    let refScore: AngularFireObject<Score>;
+    let pts: number = this.getScore(period) + points;
+    switch(period) {
+      case 'day':
+        refScore = this.dayScoreRef$;
+        break;
+      case 'total':
+        refScore = this.totScoreRef$;
+        break;
+      default:
+        refScore = null;
+    }
+
+    if(refScore !== null) {
+      refScore.set({p: pts})
+        .catch(err=>
+          console.log(err));
+    }
   }
 
 }
